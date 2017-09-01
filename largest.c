8522c21ebb3a8f114431b51ed5a7544010a7a7dd
selvasakthi <30828054+selvasakthi@users.noreply.github.com>
2017-09-01 14:53:28 +0530

    Create largest.c

---
+++ largest.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int x,y,z;
+print("enter the value of x,y,z");
+scanf("%d",&x,&y,&z);
+if(x>y)&&(x<z)
+{
+printf("x is large");
+}
+else
+if(y>z)
+{
+printf("y is large");
+}
+else
+{
+printf("z is large");
+}
