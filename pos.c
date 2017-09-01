4c1f9499f1226abca82053dcfc98173342013bc6
selvasakthi <30828054+selvasakthi@users.noreply.github.com>
2017-09-01 14:11:42 +0530

    Create pos.c

---
+++ pos.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int number;
+
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    if (number<=0)
+    {
+           
+       printf("You entered a negative number.");
+    }
+    else
+        printf("You entered a positive number.");
+    return 0;
+}
