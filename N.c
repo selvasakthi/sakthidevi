f421133ea210a03379bf1bebddb02f09a13e5be1
selvasakthi <30828054+selvasakthi@users.noreply.github.com>
2017-09-05 16:39:22 +0530

    Create N.c

---
+++ N.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+  int Num,i,Sum = 0;
+  
+  printf("\nPlease Enter any Integer Value\n");
+  scanf("%d",&Numb);
+  
+  for(i = 1; i<=Number; i++)
+  {
+     Sum =Sum + i;
+  }
+  
+  printf("Sum of Natural Numbers = %d", Sum);
+  return 0;
+}
