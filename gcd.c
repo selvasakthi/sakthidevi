834b4981905f68de57eb1e30753ee185b6feab55
selvasakthi <30828054+selvasakthi@users.noreply.github.com>
2017-09-06 17:27:41 +0530

    Update gcd.c

--- gcd.c
+++ gcd.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
-int gcd(int a, int b)
+int gcd(int x, int y)
 { 
-    if (a == 0 || b == 0)
+    if (x== 0||y == 0)
        return 0;
-    if (a == b)
-        return a;
-    if (a > b)
-        return gcd(a-b, b);
-    return gcd(a, b-a);
+    if (x== y)
+        return x;
+    if (x>y)
+        return gcd(x-y, y);
+    return gcd(x, y-x);
 }
 int main()
 {
-    int a = 98, b = 56;
-    printf("GCD of %d and %d is %d ", a, b, gcd(a, b));
+    int x= 16, y=4;
+    printf("GCD of %d and %d is %d ",x,y, gcd(x,y));
     return 0;
 }
