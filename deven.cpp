baa74a4b41b45f642209df1c29c73f951a5f089d
selvasakthi <30828054+selvasakthi@users.noreply.github.com>
2017-09-06 16:23:33 +0530

    Create deven.cpp

---
+++ deven.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+int main()
+{
+ int k;
+ int l;
+ cout<<"please enter num"<<\n;
+cin>>k;
+cout<<"please enter num"<<\n;
+cin>>l;
+while(k<l)
+{
+  if(k%2==0)
+ {
+ cout<<"even num. ="<<k<<\n;
+ }
+ k=k+1;
+}
+}
