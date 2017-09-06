b381d77d204dbb787993140905f048b2949b7571
selvasakthi <30828054+selvasakthi@users.noreply.github.com>
2017-09-06 16:41:55 +0530

    Create fact.cpp

---
+++ fact.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+int fact(int n);
+int main()
+{
+    int n;
+    cout << "Enter a positive integer: ";
+    cin >> n;
+    cout << "Fact of " << n << " = " << fact(n);
+    return 0;
+}
+int fact(int n)
+{
+    if(n > 1)
+        return n*fact(n - 1);
+    else
+        return 1;
+}
