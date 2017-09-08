3a3ce4312650606ada3c1eb6ef8cb7009cb403f9
selvasakthi <30828054+selvasakthi@users.noreply.github.com>
2017-09-08 16:07:56 +0530

    Create line.c

---
+++ line.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+void main()
+{
+    char sa[100];
+    int count = 0,k;
+    printf("enter the string\n");
+    scanf("%[^\n]a",a);
+    for (i = 0;a[i] != '\0';i++)
+    {
+        if (a[k] == ' ')
+            count++;    
+    }
+    printf("number of words in given string are: %d\n", count + 1);
+}
