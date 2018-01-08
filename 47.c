37e617905922068121da505c8e6c7e84e833c8f4
selvasakthi <30828054+selvasakthi@users.noreply.github.com>
2018-01-08 10:57:26 +0000

    Create 47.c

---
+++ 47.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int a[i],b=0;
+printf("enter the value");
+scanf("%d",a[i]);
+for(i=0;i=a[i];i++)
+{
+if(a[i]>b)
+{
+b=a[i];
+printf("the largest number is \n");
+}
+if(a[i]<b)
+{
+b=a[i];
+printf("the smallest number is \n");
+}
+}
+}
