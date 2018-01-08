8a415a9bf5181c3b5d605b408976698d8f522f0b
selvasakthi <30828054+selvasakthi@users.noreply.github.com>
2018-01-08 10:26:46 +0000

    Create 49.c

---
+++ 49.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+printf("enter the a value");
+scanf("%d",&a);
+printf("enter the b value");
+scanf("%d",&b);
+c=a+b;
+printf("the value of c is");
+getch();
+}
