ec90c764e98b3f444fde9e6b6b5f087729c5bd77
selvasakthi <30828054+selvasakthi@users.noreply.github.com>
2018-01-10 11:02:46 +0000

    Update and rename 44.c to 444.c

--- 444.c
+++ 444.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 int main()
 {
-int i,s;
+int i,g;
 printf("enter the value of i");
 scanf("%d",&i);
 for(i=1;i<=10;i++)
 {
 printf("%d",i);
 }
-if(s==i)
+if(g==i)
 {
 pritf("yes");
 }
