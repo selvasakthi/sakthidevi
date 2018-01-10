fa3bea161cb54c82bbe84ef7a2840687311831ce
selvasakthi <30828054+selvasakthi@users.noreply.github.com>
2018-01-10 10:31:08 +0000

    Update 45.cpp

--- 45.cpp
+++ 45.cpp
@@ -3,13 +3,13 @@ void main()
 {
 int n,a=0;
 clrscr();
-cout<<"Enter num value : ";
+cout<<"Enter num value :";
 cin>>n;
 while(n>0)
 {
 n=n/10;
 a++;
 }
-cout<<"\n no. of digits in given number is: "<<a;
+cout<<"no. of digits in given number is:"<<a;
 getch();
 }
