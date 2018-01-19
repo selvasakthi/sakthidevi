1d0aa4fc6fc99f95c548f33fe30766c1d6de201e
selvasakthi <30828054+selvasakthi@users.noreply.github.com>
2018-01-19 10:32:23 +0000

    Create 52.c

---
+++ 52.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+int a;
+printf("enter your choice");
+scanf("%d",&a);
+switch(a)
+case1:printf("one");
+      break;
+case2:printf("two");
+      break;
+case3:printf("three");
+      break;
+case4:printf("four');
+      break;
+case5:printf("five");
+      break;
+default:printf("error");
+       break;
+getch();
+}
