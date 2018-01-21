d02225ea7bc032685f62f892268c5c931ad24477
selvasakthi <30828054+selvasakthi@users.noreply.github.com>
2018-01-21 13:51:33 +0530

    Create 56.c

---
+++ 56.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char a[50];
+int n,i,x,y,z;
+printf("enter the string value");
+scanf("%s",&a);
+n=strlen(a);
+for(i=0;i<n;i++)
+{
+if((a[i]="0")||(a[i]="1")||(a[i]="2")||(a[i]="3")))
+{
+x=1;
+}
+if((a[i]="h")||(a[i]="e")||(a[i]="l")||(a[i]="l"||(a[i]="o")||(a[i]="w")||(a[i]="o")||(a[i]="r")||(a[i]="l")||(a[i]="d"))
+{
+y=1;
+}
+}
+z=x+y;
+if(z==2)
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
