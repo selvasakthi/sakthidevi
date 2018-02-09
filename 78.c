d3785d0603a80ced3d91f1c73cd86a25a0f6c7ca
selvasakthi <30828054+selvasakthi@users.noreply.github.com>
2018-02-09 15:16:17 +0530

    Create 78.c

---
+++ 78.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int n;
+    printf("enter the value");
+    scanf("%d",&n);
+    if(n%13==0)
+    {
+        printf("it is divided by 13");
+    }
+    else
+    {
+        printf("it is not divided by 13");
+    }
+}
