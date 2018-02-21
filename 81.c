ad9ef742b77b406e715727e5ac9b3620c9050415
selvasakthi <30828054+selvasakthi@users.noreply.github.com>
2018-02-21 19:54:28 +0530

    Create 81.c

---
+++ 81.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() 
+{
+	int kabali,opponent,i;
+	printf("enter the value");
+	for(i=0;i<=3;i++)
+	{
+           scanf("%d %d",&kabali,&opponent);
+	{
+	if(kabali>opponent)
+	{
+		printf("\n%d",kabali-opponent);
+	}
+	else
+	{
+		printf("\n%d",opponent-kabali);
+	}
+	}
+	
+}
+}
