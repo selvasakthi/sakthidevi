1d72d8ac78398757df05acc14eb593ba8a4ce0d2
selvasakthi <30828054+selvasakthi@users.noreply.github.com>
2018-02-28 16:54:51 +0530

    Update and rename 97.c to 097.c

---
+++ 097.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    char b[100];
+    int l,i;
+    printf("enter the value");
+    scanf("%s",&b);
+    l=strlen(b);
+    for(i=l-1;i>=0;i--)
+    {
+        printf("%c",b[i]);
+    }
+}
+,#include<stdio.h>
+#include<string.h>
+void main()
+{
+    char b[100];
+    int l,i;
+    printf("enter the value");
+    scanf("%s",&b);
+    l=strlen(b);
+    for(i=l-1;i>=0;i--)
+    {
+        printf("%c",b[i]);
+    }
+}
+,
