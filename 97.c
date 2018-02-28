f50106d109a47f3d79918a5592b65711b18c9947
selvasakthi <30828054+selvasakthi@users.noreply.github.com>
2018-02-28 16:54:05 +0530

    Create 97.c

---
+++ 97.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    char b[100];
+    int l,i;
+    printf("enter the value");
+    scanf("%s",&b);
+    l=strlen(b);
+    for(i=l-1;i>=0;i--)
+    {
+        printf("%c",b[i]);
+    }
+}
+,
