8d302d719f4849dcf2876f54bca2527a221d91fc
selvasakthi <30828054+selvasakthi@users.noreply.github.com>
2018-03-07 16:02:14 +0530

    Create 101.c

---
+++ 101.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int n,d;
+    printf("enter the value");
+    scanf("%d",&n);
+        if(n!=0)
+        {
+            d=n%10;
+            printf("the value is %d",d);
+        }
+     return 0;
+}
